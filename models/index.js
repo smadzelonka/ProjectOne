a267afee0369671fa55aaf74873be28ad4ea5650
Doireann Herold <ivydoor@icloud.com>
2020-12-23 12:34:03 -0800

    final css changes

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 /* === Connect === */
-const dbUrl = "mongodb://localhost:27017/curate" || process.env.MONGODB_URI;  
-
+const dbUrl = process.env.MONGODB_URI;
+/*  "mongodb://localhost:27017/curate" */ // connnect
 mongoose
   .connect(dbUrl, {
     useNewUrlParser: true,
