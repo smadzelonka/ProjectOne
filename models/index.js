26dec291bb7a8c98d8b7621bc7da61006fb3e8e0
smadzelonka <smadzelonka@gmail.com>
2020-12-23 10:17:32 -0600

    server changes

--- models/index.js
+++ models/index.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 /* === Connect === */
-const dbUrl =
-  // process.env.MONGODB_URI 
-   "mongodb://localhost:27017/curate";
-
-// connnect
+const dbUrl = process.env.MONGODB_URI;
+/*  "mongodb://localhost:27017/curate" */ // connnect
 mongoose
   .connect(dbUrl, {
     useNewUrlParser: true,
