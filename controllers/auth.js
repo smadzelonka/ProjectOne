dc571138b811c698786b9f51672880fd7913b801
smadzelonka <smadzelonka@gmail.com>
2020-12-28 22:42:40 -0600

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,8 +27,8 @@ passport.use(
   new GoogleStrategy(
     {
       callbackURL:
-        "http://localhost:4001/auth/google/artshow" ||
-        "https://localartwork.herokuapp.com/auth/google/artshow",
+        "https://localartwork.herokuapp.com/auth/google/artshow" ||
+        "http://localhost:4001/auth/google/artshow",
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
     },
